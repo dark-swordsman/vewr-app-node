96eb4a35ee12c0cdb50f23dce127135711c45a5f
dark-swordsman <kylebarr54321@gmail.com>
2019-04-02 01:55:47 -0400

    updated App.js with routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import ReactPlayer from 'react-player';
-import NavBar from './components/navbar';
-import { Typography } from '@material-ui/core';
-import CommentBox from './components/commentbox';
+
+import {
+	Router,
+	Route,
+	IndexRoute,
+	browserHistory
+} from 'react-router/lib';
+
+import Layout from './components/Layout';
+
+import Pages from './pages';
 
 class App extends Component {
 	render() {
 		return (
-			<div className='leftSide'>
-				<NavBar />
-				<div className='videoPlayer'>
-					<div className='textBeforeVid'>
-						<Typography component='h2' variant='display2'>
-							Vewr Pre-Alpha
-						</Typography>
-						<Typography component='h2' variant='caption'>
-							Posted by Abraham Mitchell
-						</Typography>
-					</div>
-					<div className='video'>
-						<ReactPlayer width='800px' height='450px' controls url='https://ipfstube.erindachtler.me/ipfs/QmU1GSqu4w29Pt7EEM57Lhte8Lce6e7kuhRHo6rSNb2UaC' />
-					</div>
-					<div className='commentBox'>
-						<CommentBox />
-					</div>
-				</div>
-			</div>
+			<Router history={browserHistory}>
+				<Route path="/" component={Layout}>
+					<IndexRoute component={Pages.Home} />
+
+					<Route path="/video" component={Pages.Video} />
+				</Route>
+			</Router>
 		);
 	}
 }
