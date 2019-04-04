2a7a943c3cc6a0bd7d4e3a6a8b9052201c7dc0fc
dark-swordsman <kylebarr54321@gmail.com>
2019-04-04 03:38:18 -0400

    got uploader working. Need to finish with other pieces

--- src/components/UploadWidget.jsx
+++ src/components/UploadWidget.jsx
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import IPFS from 'ipfs';
+import bufferImport from 'buffer/';
 
+const Buffer = bufferImport.Buffer;
 
 const styles = theme => ({
 	root: {
@@ -21,56 +24,69 @@ const styles = theme => ({
 	},
 });
 
-const message = 'test';
 class uploadWidget extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			title: '',
+			description: '',
+			ipfsLinks: [],
+		}
   }
   
-//   uploadVideo() {
-//     new Promise((resolve, reject) => {
-//       const ipfs = new Ipfs({ repo: 'ipfs-' + Math.random() });
+  uploadVideo(files) {
+		const { ipfsLinks } = this.state;
+    new Promise((resolve, reject) => {
+      const node = new IPFS({ repo: 'ipfs-' + Math.random() });
+			const reader = new FileReader();
   
-//       ipfs.once('ready', () => {
-//         const reader = new FileReader();
-//         reader.onloadend = function() {
-          
-//           const buf = buffer.Buffer(reader.result) // Convert data into buffer
-//           ipfs.add(buf, (err, result) => { // Upload buffer to IPFS
-//             if(err) {
-//               console.error(err)
-//               reject(err);
-//             }
-//             this.setState({ url: `https://ipfstube.erindachtler.me/ipfs/${result[0].hash}` })
+			node.once('ready', () => {
+				
+				reader.addEventListener('load', (e) => {
+					const video = Buffer.from(e.target.result);
+					
+					node.add(video, (err, result) => { // Upload buffer to IPFS
+						if(err) {
+							console.error(err)
+							return reject(err);
+						}
 
-//             resolve();
-//           })
-//         }
-//       })
-//     })
-//     .then((url) => {
-//       return rest.post('/video', this.state);
-//     })
-//     .then((response) => {
-//       this.props.router.push(`/video/${response.ipfsLinks[0]}`);
-//     });
-//   }
+						ipfsLinks.push(`https://ipfstube.erindachtler.me/ipfs/${result[0].hash}`);
+						this.setState({ ipfsLinks });
+						
+						resolve();
+					});
+				});
 
+				reader.readAsArrayBuffer(files[0]) // Convert data into buffer
+			});
+			
+		})
+    .then(() => {
+			// TODO: implment post to actually upload video
+      // return rest.post('/video', this.state);
+    })
+    // .then((response) => {
+    //   this.props.router.push(`/video/${response.ipfsLinks[0]}`);
+    // });
+  }
 
 	render() {
 		const { classes } = this.props;
 		return (
 		<div>
-				 <input
-        accept="image/*"
-        className={classes.input}
-        id="outlined-button-file"
-        multiple
-        type="file"
-      />
+			<input
+				accept="video/*"
+				className={classes.input}
+				id="outlined-button-file"
+				multiple
+				type="file"
+				onChange={(e) => this.uploadVideo(e.target.files)}
+			/>
       <label htmlFor="outlined-button-file">
-        <Button variant="outlined" component="span" className={classes.button} onClick={() => this.uploadVideo()}>
-          Upload
+        <Button variant="outlined" component="span" className={classes.button}>
+					Upload
         </Button>
       </label>
 			
